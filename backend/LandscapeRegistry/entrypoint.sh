1f4c49597e1aa8da6bacfec0622e01d65023274d
Maximilian Csuk <maximilian.csuk@gmx.at>
2020-04-27 09:53:30 +0200

    Bugfix

--- backend/LandscapeRegistry/entrypoint.sh
+++ backend/LandscapeRegistry/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-./DBMigrate/DBMigrate $ConnectionStrings__LandscapeDatabaseConnection
+./DBMigrate/DBMigrate "$ConnectionStrings__LandscapeDatabaseConnection"
 
 dotnet LandscapeRegistry.dll
