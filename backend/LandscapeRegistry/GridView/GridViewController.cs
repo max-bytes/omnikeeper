84851336d840d6fefee1a4097f5b55341014f144
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2020-10-02 10:32:39 +0200

    some mediatR implementations

---
+++ backend/LandscapeRegistry/GridView/GridViewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandscapeRegistry.GridView
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GridViewController : ControllerBase
+    {
+
+    }
+}
