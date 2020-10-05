a9db259ada534580fab3a8dc8fb4616086f9b91f
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2020-10-05 09:19:12 +0200

    small changes

---
+++ backend/LandscapeRegistry/GridView/Queries/GetPredicatesQuery.cs
@@ -0,0 +1,22 @@
+﻿using LandscapeRegistry.GridView.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandscapeRegistry.GridView.Queries
+{
+    public class GetPredicatesQuery
+    {
+        public class Query : IRequest<GetPredicatesResponse>
+        {
+
+        }
+
+        //public class QueryHandler : 
+        //{
+
+        //}
+    }
+}
