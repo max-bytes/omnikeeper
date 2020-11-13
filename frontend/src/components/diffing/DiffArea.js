bd8b647e05216f3d0fe11b0e2ccd0d4c868e8c96
Benjamin Kurz <benjamin.kurz@mhx-operations.at>
2020-11-13 16:45:09 +0100

    removed unused import

--- frontend/src/components/diffing/DiffArea.js
+++ frontend/src/components/diffing/DiffArea.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import _ from 'lodash';
 import DiffAttributeList from 'components/diffing/DiffAttributeList';
 import DiffRelationList from 'components/diffing/DiffRelationList';
