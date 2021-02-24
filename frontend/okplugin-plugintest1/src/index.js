2e6e6c944c88391c7c62b44f0a910d1b6abacf69
Benjamin Kurz <benjamin.kurz@mhx-operations.at>
2021-02-24 14:44:40 +0100

    removed create-react-files, restored old structure and added prePublishOnly-information to package.json

--- frontend/okplugin-plugintest1/src/index.js
+++ frontend/okplugin-plugintest1/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default (wantedVersion) => {
+    return class extends Component {
+        render() {
+            return (
+                <div>
+                    <h1>Plugin: okplugin-plugintest1</h1>
+                    <p>Wanted version from Core: {wantedVersion ? wantedVersion : "/"}</p>
+                    <Button onClick={() => alert("'Hello' back from plugin 1")}>Say 'Hello'</Button>
+                </div>
+            )
+        }
+    }
+}
