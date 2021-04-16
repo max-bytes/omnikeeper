97e0bbbf16e93e806a7e40e1ee4f87a8d620fa9b
Maximilian Csuk <maximilian.csuk@gmx.at>
2021-04-16 18:37:20 +0200

    Initial implementation of generating go client library during build

---
+++ clients/scripts/go/generate_publish.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+git_user_id=$1
+git_repo_id=$2
+release_note=$3
+git_host=$4
+swagger_file=$5
+
+if [ "$git_host" = "" ]; then
+    echo "[INFO] No git_host provided."
+    exit -1
+fi
+
+if [ "$git_user_id" = "" ]; then
+    echo "[INFO] No git_user_id provided."
+    exit -1
+fi
+
+if [ "$git_repo_id" = "" ]; then
+    echo "[INFO] No git_repo_id provided."
+    exit -1
+fi
+
+if [ "$release_note" = "" ]; then
+    echo "[INFO] No release_note provided."
+    exit -1
+fi
+
+if [ "$swagger_file" = "" ]; then
+    echo "[INFO] No swagger_file provided."
+    exit -1
+fi
+
+rm -rf build/go
+mkdir -p build/go
+
+# HACK: openapi-generator for go cannot deal with multipart/formdata due to a bug in the go code generator 
+# that's why we remove any endpoint that contains this
+cat $swagger_file \
+    | jq 'del(.paths[] | select(.post.requestBody.content."multipart/form-data" != null))' \
+    > build/omnikeeper_trimmed.json
+
+
+cd build/go
+
+
+# Clone the current repo
+if [ "$GIT_TOKEN" = "" ]; then
+    echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
+    git clone https://${git_host}/${git_user_id}/${git_repo_id}.git .
+else
+    git clone https://${git_user_id}:${GIT_TOKEN}@${git_host}/${git_user_id}/${git_repo_id}.git .
+fi
+
+# create updated library
+docker run --rm -v "${PWD}/..:/local" -u `id -u $USER`:`id -g $USER` openapitools/openapi-generator-cli generate \
+    -i /local/omnikeeper_trimmed.json \
+    -g go \
+    -o /local/go \
+    -p enumClassPrefix=true \
+    --git-host "${git_host}" --git-user-id "${git_user_id}" --git-repo-id "${git_repo_id}.git" \
+    --global-property=verbose=true \
+    --additional-properties=packageName=okclient,packageVersion=1.0.0
+# Flag -p enumClassPrefix=true is necessary to avoid enum name clashes
+# Flags --git-* are necessary so that the generated go.mod file contains the correct package definition
+
+# Adds the files in the local repository and stages them for commit.
+git add .
+
+# Commits the tracked changes and prepares them to be pushed to a remote repository.
+git commit -m "$release_note"
+
+# Pushes the changes in the local repository up to the remote repository
+git push
+
