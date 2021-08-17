43fd5ce9a6cd50a40e8b025e3225f3237ec46e56
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-08-17 15:03:19 +0200

    frontend: small improvement

--- frontend/core/src/components/Relation.js
+++ frontend/core/src/components/Relation.js
@@ -4,6 +4,7 @@ import { Link  } from 'react-router-dom'
 import _ from 'lodash';
 import LayerIcon from "./LayerIcon";
 import OriginPopup from "./OriginPopup";
+import { CIID } from "utils/uuidRenderers";
 
 const { Text } = Typography;
 
@@ -16,8 +17,8 @@ export default function Relation(props) {
     <i>{predicate.wordingFrom}</i>
   : <i style={{textDecorationStyle: 'dashed', textDecorationColor: 'red', textDecorationThickness: '1px', textDecorationLine: 'underline'}}>{relation.predicateID}</i>;
 
-  const fromCIButton = <Link to={"/explorer/" + relation.fromCIID}>{relation.fromCIID}</Link>;
-  const toCIButton = <Link to={"/explorer/" + relation.toCIID}>{relation.toCIID}</Link>;
+  const fromCIButton = <CIID id={relation.fromCIID} link={true} />;
+  const toCIButton = <CIID id={relation.toCIID} link={true} />;
 
   const removed = relation.state === 'REMOVED';
   const written = <span>{fromCIButton}{` `}{predicateWording}{` `}{toCIButton}</span>;
@@ -28,7 +29,7 @@ export default function Relation(props) {
       <Form layout="inline" style={{flexFlow: 'nowrap', alignItems: 'center'}}>
         <LayerIcon layer={layer} />
         <OriginPopup changesetID={relation.changesetID} />
-        <Form.Item style={{flexBasis: '600px', justifyContent: 'flex-start', paddingRight: "0.25rem"}}>{wrapped}</Form.Item>
+        <Form.Item style={{flexBasis: '800px', justifyContent: 'flex-start', paddingRight: "0.25rem"}}>{wrapped}</Form.Item>
       </Form>
     </div>
   );
