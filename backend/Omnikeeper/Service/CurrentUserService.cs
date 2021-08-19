e0a89acf6cbf45b67155ba900682caae07515082
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-08-19 13:01:42 +0200

    Treat missing roles key in auth token as warning, not as exception

--- backend/Omnikeeper/Service/CurrentUserService.cs
+++ backend/Omnikeeper/Service/CurrentUserService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using Omnikeeper.Base.Entity;
 using Omnikeeper.Base.Model;
@@ -18,17 +19,19 @@ namespace Omnikeeper.Service
     {
 
         public CurrentUserService(IHttpContextAccessor httpContextAccessor, IUserInDatabaseModel userModel, ILayerModel layerModel,
-            IAuthRoleModel authRoleModel, IConfiguration configuration)
+            IAuthRoleModel authRoleModel, IConfiguration configuration, ILogger<CurrentUserService> logger)
         {
             HttpContextAccessor = httpContextAccessor;
             UserModel = userModel;
             LayerModel = layerModel;
             AuthRoleModel = authRoleModel;
             Configuration = configuration;
+            Logger = logger;
         }
 
         private IAuthRoleModel AuthRoleModel { get; }
         private IConfiguration Configuration { get; }
+        public ILogger<CurrentUserService> Logger { get; }
         private IHttpContextAccessor HttpContextAccessor { get; }
         private IUserInDatabaseModel UserModel { get; }
         public ILayerModel LayerModel { get; }
@@ -81,11 +84,16 @@ namespace Omnikeeper.Service
                 }
                 var resourceName = Configuration.GetSection("Authentication")["Audience"];
                 var claimRoles = resourceAccess[resourceName]?["roles"];
+                var clientRoles = new HashSet<string>();
                 if (claimRoles == null)
                 {
-                    throw new Exception($"Cannot parse roles in user token: key-path \"resource_access\"->\"{resourceName}\"->\"roles\" not found");
+                    Logger.LogWarning($"Cannot parse roles in user token for user {username}: key-path \"resource_access\"->\"{resourceName}\"->\"roles\" not found; either no roles assigned or token structure invalid");
+
                 }
-                var clientRoles = claimRoles.Select(tt => tt.Value<string>()).ToArray() ?? new string[] { };
+                else
+                {
+                    clientRoles = claimRoles.Select(tt => tt.Value<string>()).ToHashSet();
+                }
 
                 var usertype = UserType.Unknown;
                 if (clientRoles.Contains("human"))
