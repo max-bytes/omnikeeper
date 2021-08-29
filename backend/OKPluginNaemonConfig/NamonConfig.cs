89f17ea5dc12c6965051e9923facf2d662af1447
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2021-08-29 16:38:24 +0200

    add NaemonConfig plugin

---
+++ backend/OKPluginNaemonConfig/NamonConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Omnikeeper.Base.CLB;
+using Omnikeeper.Base.Entity;
+using Omnikeeper.Base.Model;
+using Omnikeeper.Base.Utils.ModelContext;
+using System;
+using System.Threading.Tasks;
+
+namespace OKPluginNaemonConfig
+{
+    public class NamonConfig : CLBBase
+    {
+        private readonly IRelationModel relationModel;
+        private readonly ICIModel ciModel;
+        private readonly IEffectiveTraitModel traitModel;
+        public NamonConfig(ICIModel ciModel, IAttributeModel atributeModel, ILayerModel layerModel, IEffectiveTraitModel traitModel, IRelationModel relationModel,
+                           IChangesetModel changesetModel, IUserInDatabaseModel userModel)
+            : base(atributeModel, layerModel, changesetModel, userModel)
+        {
+            this.ciModel = ciModel;
+            this.relationModel = relationModel;
+            this.traitModel = traitModel;
+        }
+        public override async Task<bool> Run(Layer targetLayer, IChangesetProxy changesetProxy, CLBErrorHandler errorHandler, IModelContext trans, ILogger logger)
+        {
+            logger.LogDebug("Start naemonConfig");
+
+            // function getNaemonInstanceById
+            return true;
+        }
+    }
+}
---
+++ backend/OKPluginNaemonConfig/PluginRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Omnikeeper.Base.CLB;
+using Omnikeeper.Base.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OKPluginNaemonConfig
+{
+    public class PluginRegistration : PluginRegistrationBase
+    {
+        public override void RegisterServices(IServiceCollection sc)
+        {
+            sc.AddSingleton<IComputeLayerBrain, NamonConfig>();
+        }
+
+        //public override IEnumerable<RecursiveTrait> DefinedTraits => Traits.RecursiveTraits;
+    }
+}
