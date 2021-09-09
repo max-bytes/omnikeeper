fc322242cdfe475f1199e9d4fef7d34233f5a677
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-09-09 10:41:31 +0200

    Tiny changes to perf tests

--- backend/PerfTests/GetMergedAttributesTest.cs
+++ backend/PerfTests/GetMergedAttributesTest.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
+using Omnikeeper.Base.Entity;
 using Omnikeeper.Base.Entity.DataOrigin;
 using Omnikeeper.Base.Model;
 using Omnikeeper.Base.Utils;
@@ -20,11 +21,8 @@ namespace PerfTests
     [Explicit]
     public class GetMergedAttributesTest : Base
     {
-        private List<Guid> ciids = new List<Guid>();
-        private List<string> layerNames = new List<string>();
-        private IModelContextBuilder? modelContextBuilder;
         private IAttributeModel? attributeModel;
-        private Omnikeeper.Base.Entity.LayerSet? layerset;
+        private LayerSet? layerset;
         private ICIIDSelection? specificCIIDs;
         private ICIIDSelection? allExceptCIIDs;
         private readonly Consumer consumer = new Consumer();
@@ -51,12 +49,20 @@ namespace PerfTests
         public async Task Setup()
         {
             Setup(WithModelCaching, WithEffectiveTraitCaching);
+
             await SetupData();
 
+            var modelContextBuilder = ServiceProvider.GetRequiredService<IModelContextBuilder>();
             var layerModel = ServiceProvider.GetRequiredService<ILayerModel>();
             attributeModel = ServiceProvider.GetRequiredService<IAttributeModel>();
             using var mc = modelContextBuilder!.BuildImmediate();
-            layerset = layerModel!.BuildLayerSet(layerNames.ToArray(), mc).GetAwaiter().GetResult();
+            var layers = await layerModel.GetLayers(mc);
+            layerset = layerModel!.BuildLayerSet(layers.Select(l => l.ID).ToArray(), mc).GetAwaiter().GetResult();
+
+            var ciidModel = ServiceProvider.GetRequiredService<ICIIDModel>();
+            var ciids = (await ciidModel.GetCIIDs(mc)).ToList();
+            specificCIIDs = SpecificCIIDsSelection.Build(ciids.Take(ciids.Count / 3).ToHashSet());
+            allExceptCIIDs = AllCIIDsExceptSelection.Build(ciids.Take(ciids.Count / 3).ToHashSet());
         }
 
         [Benchmark]
@@ -64,7 +70,8 @@ namespace PerfTests
         {
             BaseAttributeModel._USE_LATEST_TABLE = UseLatestTable;
 
-            using var mc = modelContextBuilder!.BuildImmediate();
+            var modelContextBuilder = ServiceProvider.GetRequiredService<IModelContextBuilder>();
+            using var mc = modelContextBuilder.BuildImmediate();
 
             var selection = CIIDSelection switch
             {
@@ -77,27 +84,47 @@ namespace PerfTests
             (await attributeModel!.GetMergedAttributes(selection, layerset!, mc, TimeThreshold.BuildLatest())).Consume(consumer);
         }
 
+        [Test]
+        public void RunBenchmark()
+        {
+            var summary = BenchmarkRunner.Run<GetMergedAttributesTest>();
+        }
+
+
+        [Test]
+        public async Task RunDebuggable()
+        {
+            CIIDSelection = "specific";
+            WithModelCaching = false;
+            WithEffectiveTraitCaching = false;
+            UseLatestTable = true;
+            AttributeCITuple = AttributeCITuples.First();
+            await Setup();
+            await GetMergedAttributes();
+            TearDown();
+        }
+
         [GlobalCleanup(Target = nameof(GetMergedAttributes))]
         public void TearDownT() => TearDown();
 
         private async Task SetupData()
         {
             var changesetModel = ServiceProvider.GetRequiredService<IChangesetModel>();
+            var modelContextBuilder = ServiceProvider.GetRequiredService<IModelContextBuilder>();
             var layerModel = ServiceProvider.GetRequiredService<ILayerModel>();
             var attributeModel = ServiceProvider.GetRequiredService<IAttributeModel>();
             var ciModel = ServiceProvider.GetRequiredService<ICIModel>();
             var userModel = ServiceProvider.GetRequiredService<IUserInDatabaseModel>();
-            modelContextBuilder = ServiceProvider.GetRequiredService<IModelContextBuilder>();
             var user = await DBSetup.SetupUser(userModel, modelContextBuilder.BuildImmediate());
 
             var random = new Random(3);
 
-            ciids = Enumerable.Range(0, AttributeCITuple.numCIs).Select(i =>
+            var ciids = Enumerable.Range(0, AttributeCITuple.numCIs).Select(i =>
             {
                 return Guid.NewGuid();
             }).ToList();
 
-            layerNames = Enumerable.Range(0, AttributeCITuple.numLayers).Select(i =>
+            var layerNames = Enumerable.Range(0, AttributeCITuple.numLayers).Select(i =>
             {
                 var identity = "l_" + RandomUtility.GenerateRandomString(8, random, "abcdefghijklmnopqrstuvwxy");
                 return identity;
@@ -111,23 +138,39 @@ namespace PerfTests
                 return (ciModel.CreateCI(identity, mc).GetAwaiter().GetResult(), identity);
             }).ToList();
 
-            specificCIIDs = SpecificCIIDsSelection.Build(ciids.Take(ciids.Count / 3).ToHashSet());
-            allExceptCIIDs = AllCIIDsExceptSelection.Build(ciids.Take(ciids.Count / 3).ToHashSet());
-
             var layers = layerNames.Select(identity =>
             {
                 return layerModel.UpsertLayer(identity, mc).GetAwaiter().GetResult();
             }).ToList();
 
             var attributeNames = Enumerable.Range(0, AttributeCITuple.numAttributeNames).Select(i => "A" + RandomUtility.GenerateRandomString(32, random)).ToList();
-            var attributes = Enumerable.Range(0, AttributeCITuple.numAttributeInserts).Select(i =>
+            foreach (var layer in layers)
             {
-                var name = attributeNames.GetRandom(random);
-                var value = new AttributeScalarValueText("V" + RandomUtility.GenerateRandomString(8, random));
-                var layer = layers.GetRandom(random);
-                var ciid = cis.GetRandom(random).Item1;
-                return attributeModel.InsertAttribute(name!, value, ciid, layer!.ID, changeset, new DataOriginV1(DataOriginType.Manual), mc).GetAwaiter().GetResult();
-            }).ToList();
+                var usedAttributes = new HashSet<string>();
+                var fragments = Enumerable.Range(0, AttributeCITuple.numAttributeInserts).Select(i =>
+                {
+                    var found = false;
+                    string name = "";
+                    Guid ciid = new Guid();
+                    while (!found)
+                    {
+                        name = attributeNames.GetRandom(random)!;
+                        ciid = cis.GetRandom(random).Item1;
+                        var hash = CIAttribute.CreateInformationHash(name, ciid);
+                        if (!usedAttributes.Contains(hash))
+                        {
+                            usedAttributes.Add(hash);
+                            found = true;
+                        }
+                    }
+                    var value = new AttributeScalarValueText("V" + RandomUtility.GenerateRandomString(8, random));
+                    return new BulkCIAttributeDataLayerScope.Fragment(name, value, ciid);
+                });
+
+                var data = new BulkCIAttributeDataLayerScope("", layer!.ID, fragments);
+
+                await attributeModel.BulkReplaceAttributes(data, changeset, new DataOriginV1(DataOriginType.Manual), mc);
+            }
 
             mc.Commit();
         }
