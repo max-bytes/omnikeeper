785a8739332a1d32f3720f701c75c77309e92178
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-09-09 11:01:00 +0200

    Removed unused and outdated code

--- backend/Omnikeeper.Base/Service/RelationService.cs
+++ backend/Omnikeeper.Base/Service/RelationService.cs
@@ -11,42 +11,6 @@ namespace Omnikeeper.Base.Service
 {
     public class RelationService
     {
-        //public static async Task<IEnumerable<CompactRelatedCI>> GetCompactRelatedCIs(LayerSet layerset, ICIIDSelection ciidSelection, ICIModel ciModel, IRelationModel relationModel, IModelContext trans, TimeThreshold atTime)
-        //{
-        //    var relations = await relationModel.GetMergedRelations(new RelationSelectionEitherFromOrTo(ciid), layerset, trans, atTime);
-
-        //    // HACK: limit number per predicate type
-        //    var predicateCounts = new Dictionary<string, int>();
-
-        //    var relationTuples = relations.Select(r =>
-        //    {
-        //        var isForwardRelation = r.Relation.FromCIID == ciid;
-        //        var relatedCIID = (isForwardRelation) ? r.Relation.ToCIID : r.Relation.FromCIID;
-        //        return (relation: r, relatedCIID, isForwardRelation);
-        //    });
-
-
-        //    var relatedCIs = new List<CompactRelatedCI>();
-
-        //    if (!relationTuples.IsEmpty())
-        //    {
-        //        var relatedCompactCIs = (await ciModel.GetCompactCIs(SpecificCIIDsSelection.Build(relationTuples.Select(t => t.relatedCIID).ToHashSet()), layerset, trans, atTime))
-        //            .ToDictionary(ci => ci.ID); // TODO: performance improvements
-        //        foreach ((var relation, var relatedCIID, var isForwardRelation) in relationTuples)
-        //        {
-        //            var predicateID = relation.Relation.PredicateID;
-        //            var changesetID = relation.Relation.ChangesetID;
-        //            if (relatedCompactCIs.TryGetValue(relatedCIID, out var ci)) // TODO: performance improvements
-        //                relatedCIs.Add(new CompactRelatedCI(ci, relation.Relation.ID, relation.Relation.FromCIID, relation.Relation.ToCIID, changesetID, predicateID, isForwardRelation, relation.LayerStackIDs));
-        //        }
-        //    }
-
-        //    return relatedCIs;
-        //}
-
-
-
-
         public static async Task<IEnumerable<CompactRelatedCI>> GetCompactRelatedCIs(Guid ciid, LayerSet layerset, ICIModel ciModel, IRelationModel relationModel, int? perPredicateLimit, IModelContext trans, TimeThreshold atTime)
         {
             var relations = await relationModel.GetMergedRelations(new RelationSelectionEitherFromOrTo(ciid), layerset, trans, atTime);
