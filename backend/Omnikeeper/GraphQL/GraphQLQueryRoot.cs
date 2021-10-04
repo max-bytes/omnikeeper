ee7ef000b28b5618d8557997c9d33bdeebb1f1b1
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-10-04 09:14:33 +0200

    small refactoring

--- backend/Omnikeeper/GraphQL/GraphQLQueryRoot.cs
+++ backend/Omnikeeper/GraphQL/GraphQLQueryRoot.cs
@@ -311,13 +311,4 @@ namespace Omnikeeper.GraphQL
                 });
         }
     }
-
-    public static class UserContextExtensions
-    {
-        public static OmnikeeperUserContext SetupUserContext(this IResolveFieldContext<object> rfc)
-        {
-            var userContext = (rfc.UserContext as OmnikeeperUserContext)!;
-            return userContext;
-        }
-    }
 }
---
+++ backend/Omnikeeper/GraphQL/UserContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL;
+
+namespace Omnikeeper.GraphQL
+{
+    public static class UserContextExtensions
+    {
+        public static OmnikeeperUserContext SetupUserContext(this IResolveFieldContext<object> rfc)
+        {
+            var userContext = (rfc.UserContext as OmnikeeperUserContext)!;
+            return userContext;
+        }
+    }
+}
