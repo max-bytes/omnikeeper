08b2c0713efee8f3d3e8dd10c154b2d603a7bcd9
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-11-15 09:19:27 +0100

    Added auth redirect REST endpoint to point clients to proper oauth URLs

---
+++ backend/Omnikeeper/Controllers/AuthRedirectController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Omnikeeper.Controllers
+{
+    [ApiController]
+    [ApiVersionNeutral]
+    [Route(".well-known/openid-configuration")]
+    public class AuthRedirectController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public AuthRedirectController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var baseURL = configuration.GetSection("Authentication")["Authority"];
+            return Redirect($"{baseURL}/.well-known/openid-configuration");
+        }
+    }
+}
