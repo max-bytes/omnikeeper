e777536253c72c001033aef924991cff7fa42473
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2021-11-24 10:29:56 +0100

    Removed DBMigrations from plugin generic json ingest

--- backend/OKPluginGenericJSONIngest/PluginRegistration.cs
+++ backend/OKPluginGenericJSONIngest/PluginRegistration.cs
@@ -1,10 +1,7 @@
-﻿using DBMigrations;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Omnikeeper.Base.Entity;
-using Omnikeeper.Base.Model;
 using Omnikeeper.Base.Plugins;
 using Omnikeeper.Controllers.Ingest;
-using System;
 using System.Collections.Generic;
 using Omnikeeper.Base.Model.TraitBased;
 
@@ -12,8 +9,6 @@ namespace OKPluginGenericJSONIngest
 {
     public class PluginRegistration : PluginRegistrationBase
     {
-        public override IPluginDBMigrator? DBMigration => new PluginDBMigrator();
-
         public override string? ManagementEndpoint { get; } = "manage/plugin/genericJSONIngest";
 
         public override void RegisterServices(IServiceCollection sc)
