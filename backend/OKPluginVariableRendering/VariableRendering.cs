6efb5cbc1f06cca10c5bb13d4801dc13f9d8b282
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2021-12-03 15:53:14 +0100

    check CI traits correctly

--- backend/OKPluginVariableRendering/VariableRendering.cs
+++ backend/OKPluginVariableRendering/VariableRendering.cs
@@ -17,17 +17,20 @@ namespace OKPluginVariableRendering
     {
         private readonly IRelationModel relationModel;
         private readonly ICIModel ciModel;
-        private readonly IEffectiveTraitModel traitModel;
+        private readonly ITraitsProvider traitsProvider;
+        private readonly IEffectiveTraitModel effectiveTraitModel;
         private readonly ILayerModel layerModel;
         private readonly IAttributeModel attributeModel;
-        public VariableRendering(ICIModel ciModel, ILayerModel layerModel, IEffectiveTraitModel traitModel, 
-            IRelationModel relationModel, IAttributeModel attributeModel, ILatestLayerChangeModel latestLayerChangeModel) : base(latestLayerChangeModel)
+        public VariableRendering(ICIModel ciModel, ILayerModel layerModel, IEffectiveTraitModel effectiveTraitModel,
+            IRelationModel relationModel, IAttributeModel attributeModel, ILatestLayerChangeModel latestLayerChangeModel,
+            ITraitsProvider traitsProvider) : base(latestLayerChangeModel)
         {
             this.relationModel = relationModel;
             this.ciModel = ciModel;
-            this.traitModel = traitModel;
+            this.effectiveTraitModel = effectiveTraitModel;
             this.layerModel = layerModel;
             this.attributeModel = attributeModel;
+            this.traitsProvider = traitsProvider;
         }
 
         const int moduleGroupPrio = 10;
@@ -36,11 +39,10 @@ namespace OKPluginVariableRendering
         const int networkInterfaceGroupPrio = 2000;
         const int assignmentGroupDefaultPrio = 10000;
         const int ciPrio = 100000;
-         
 
         public override async Task<bool> Run(Layer targetLayer, JObject config, IChangesetProxy changesetProxy, IModelContext trans, ILogger logger)
         {
-             logger.LogDebug("Start VariableRendering");
+            logger.LogDebug("Start VariableRendering");
 
             //return false;
 
@@ -57,7 +59,229 @@ namespace OKPluginVariableRendering
                 return false;
             }
 
+            var layersetVariableRendering = await layerModel.BuildLayerSet(cfg.InputLayerSet.ToArray(), trans);
 
+            var allCIs = await ciModel.GetMergedCIs(new AllCIIDsSelection(), layersetVariableRendering, false, AllAttributeSelection.Instance, trans, changesetProxy.TimeThreshold);
+
+            var activeTrait = await traitsProvider.GetActiveTrait(cfg.BaseCI.RequiredTrait, trans, changesetProxy.TimeThreshold);
+
+            if (activeTrait == null)
+            {
+                logger.LogError($"Could not find baseCI required trait {cfg.BaseCI.RequiredTrait}");
+                return false;
+            }
+
+            var mainCIs = await effectiveTraitModel.FilterCIsWithTrait(allCIs, activeTrait, layersetVariableRendering, trans, changesetProxy.TimeThreshold);
+
+            //TODO: select only the realtions that are defined in configuration
+            //      check if selection with specific predicates is possible
+            var relations = new List<string>();
+
+            cfg.BaseCI.FollowRelations.ForEach(r =>
+            {
+                r.Follow.ForEach(f =>
+                {
+                    if (relations.IndexOf(f.Predicate[1..]) == -1)
+                    {
+                        relations.Add(f.Predicate[1..]);
+                    }
+                });
+            });
+
+            var allRelations = new List<Relation>();
+
+            foreach (var layer in cfg.InputLayerSet)
+            {
+                var layerRelations = await relationModel.GetRelations(RelationSelectionAll.Instance, layer, trans, changesetProxy.TimeThreshold);
+
+                allRelations.AddRange(layerRelations);
+            }
+
+            foreach (var mainCI in mainCIs)
+            {
+                // NOTE use Dictionary instead list here ??
+                var gatheredAttributes = new List<GatheredAttribute>();
+
+                foreach (var followRelation in cfg.BaseCI.FollowRelations)
+                {
+                    var prevCI = mainCI;
+                    foreach (var follow in followRelation.Follow)
+                    {
+                        // get the predicate remove the first char, first char defines the direction of relation
+                        var predicate = follow.Predicate[1..];
+
+                        // check relation direction
+                        var r = allRelations.Where(r =>
+                        {
+                            if (follow.Predicate[0] == '>')
+                            {
+                                return r.FromCIID == prevCI.ID && r.PredicateID == predicate;
+                            }
+                            else
+                            {
+                                return r.ToCIID == prevCI.ID && r.PredicateID == predicate;
+                            }
+                        }).FirstOrDefault();
+
+
+                        if (r == null)
+                        {
+                            break;
+                        }
+
+                        var targetCI = allCIs.Where(ci =>
+                        {
+                            if (follow.Predicate[0] == '>')
+                            {
+                                return r.ToCIID == ci.ID;
+                            }
+                            else
+                            {
+                                return r.FromCIID == ci.ID;
+                            }
+                        }
+                        ).FirstOrDefault();
+
+                        prevCI = targetCI;
+
+                        // NOTE check target CI required trait
+                        var targetCIRequiredTrait = await traitsProvider.GetActiveTrait(follow.RequiredTrait, trans, changesetProxy.TimeThreshold);
+
+                        if (targetCIRequiredTrait == null)
+                        {
+                            logger.LogError($"The required trait {follow.RequiredTrait} for follow realtions was not found!");
+                            continue;
+                        }
+
+                        // check if target CI has the required trait
+                        var aa = await effectiveTraitModel.FilterCIsWithTrait(new List<MergedCI> { targetCI }, targetCIRequiredTrait, layersetVariableRendering, trans, changesetProxy.TimeThreshold);
+
+                        if (aa.ToList().Count == 0)
+                        {
+                            logger.LogInformation("");
+                            continue;
+                        }
+
+                        var targetCIAttributes = targetCI.MergedAttributes.Where(a => IsAttributeAllowed(a.Value.Attribute.Name, follow.InputWhitelist, follow.InputBlacklist)).ToList();
+
+                        var allCIAttributes = targetCIAttributes.Select(a => new GatheredAttribute { SourceCIID = targetCI.ID, Name = a.Key, RequiredTrait = follow.RequiredTrait, Value = a.Value.Attribute.Value.ToString() }).ToList();
+
+                        var tmpCIAttributes = new List<GatheredAttribute>();
+
+                        foreach (var mapping in follow.AttributeMapping)
+                        {
+                            foreach (var attribute in allCIAttributes)
+                            {
+                                if (IsAttributeIncludedInSource(attribute.Name, mapping.Source))
+                                {
+                                    var prio = 0;
+
+                                    switch (predicate[1..])
+                                    {
+                                        case "belongs_to_customer":
+                                            prio = customersGroupPrio;
+                                            break;
+                                        case "has_network_interface":
+                                            prio = networkInterfaceGroupPrio;
+                                            break;
+                                        case "is_attached_to":
+                                            prio = networkSegmentGroupPrio;
+                                            break;
+                                        case "is_assigned_to":
+                                            prio = moduleGroupPrio;
+                                            break;
+                                        case "belongs_to_assignment_group":
+                                            prio = assignmentGroupDefaultPrio;
+                                            break;
+                                        default:
+                                            break;
+                                    }
+
+                                    var a = new GatheredAttribute
+                                    {
+                                        SourceCIID = targetCI.ID,
+                                        Name = GetTargetName(attribute.Name, mapping.Target),
+                                        Value = attribute.Value,
+                                        RequiredTrait = follow.RequiredTrait,
+                                        Priority = prio,
+                                    };
+
+                                    var i = tmpCIAttributes.FindIndex(e => e.Name == attribute.Name);
+
+                                    if (i != -1)
+                                    {
+                                        // check if current attribute has higher priority
+                                        if (tmpCIAttributes[i].Priority < a.Priority)
+                                        {
+                                            tmpCIAttributes.RemoveAt(i);
+                                            tmpCIAttributes.Add(a);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        tmpCIAttributes.Add(a);
+                                    }
+
+                                    attribute.Name = a.Name;
+                                    attribute.Value = a.Value;
+                                }
+                            }
+                        }
+
+                        gatheredAttributes.AddRange(tmpCIAttributes);
+                    }
+                }
+
+                // for all gathered attributes insert them to the main ci
+
+                foreach (var mapping in cfg.BaseCI.AttributeMapping)
+                {
+
+                    foreach (var attribute in gatheredAttributes)
+                    {
+                        // first we need to check input whitelist and blacklist
+
+                        if (!IsAttributeAllowed(attribute.Name, cfg.BaseCI.InputWhitelist, cfg.BaseCI.InputBlacklist))
+                        {
+                            continue;
+                        }
+
+                        // check base ci attribute mapping
+
+                        if (!IsAttributeIncludedInSource(attribute.Name, mapping.Source))
+                        {
+                            continue;
+                        }
+
+                        // check if attribute exists in base ci
+
+                        if (mainCI.MergedAttributes.ContainsKey(attribute.Name) && ciPrio < attribute.Priority)
+                        {
+                            // don't change this attribute since 
+                            continue;
+                        }
+
+                        var (_, changed) = await attributeModel.InsertAttribute(
+                            GetTargetName(attribute.Name, mapping.Target),
+                            new AttributeScalarValueText(attribute.Value),
+                            mainCI.ID,
+                            cfg.TargetLayer,
+                            changesetProxy,
+                            new DataOriginV1(DataOriginType.Manual),
+                            trans);
+
+                        if (!changed)
+                        {
+                            logger.LogError($"An error ocurred trying to insert attribute for CI with id={mainCI.ID}");
+                        }
+                    }
+
+                }
+            }
+
+
+
+            /*
             foreach (var layer in cfg.InputLayerSet)
             {
                 logger.LogDebug($"Started rendering variables for layer {layer}");
@@ -81,7 +305,7 @@ namespace OKPluginVariableRendering
                     });
                 });
 
-                // TODO check if selection with specific predicates is possible than use the relations array created earlier
+               
                 var allRelations = await relationModel.GetRelations(RelationSelectionAll.Instance, layer, trans, changesetProxy.TimeThreshold);
 
                 foreach (var mainCI in mainCIs)
@@ -152,15 +376,20 @@ namespace OKPluginVariableRendering
 
                                         switch (predicate[1..])
                                         {
-                                            case "belongs_to_customer":  prio = customersGroupPrio;
+                                            case "belongs_to_customer":  
+                                                prio = customersGroupPrio;
                                                 break;
-                                            case "has_network_interface": prio = networkInterfaceGroupPrio;
+                                            case "has_network_interface": 
+                                                prio = networkInterfaceGroupPrio;
                                                 break;
-                                            case "is_attached_to": prio = networkSegmentGroupPrio;
+                                            case "is_attached_to": 
+                                                prio = networkSegmentGroupPrio;
                                                 break;
-                                            case "is_assigned_to": prio = moduleGroupPrio;
+                                            case "is_assigned_to": 
+                                                prio = moduleGroupPrio;
                                                 break;
-                                            case "belongs_to_assignment_group": prio = assignmentGroupDefaultPrio;
+                                            case "belongs_to_assignment_group":
+                                                prio = assignmentGroupDefaultPrio;
                                                 break;
                                             default:
                                                 break;
@@ -247,6 +476,7 @@ namespace OKPluginVariableRendering
                     }
                 }
             }
+            */
 
             return true;
         }
