3d4e3182a23a978386620adf9efbe9c4eac14147
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-01-26 08:48:45 +0100

    Test refactorings

--- backend/Tests/Integration/Service/ArchiveUnusedCIsServiceTest.cs
+++ backend/Tests/Integration/Service/ArchiveUnusedCIsServiceTest.cs
@@ -14,46 +14,40 @@ using System.Threading.Tasks;
 
 namespace Tests.Integration.Service
 {
-    class ArchiveUnusedCIsServiceTest : DBBackedTestBase
+    class ArchiveUnusedCIsServiceTest : DIServicedTestBase
     {
         [Test]
         public async Task TestArchiveUnusedCIs()
         {
-            var attributeModel = new AttributeModel(new BaseAttributeModel(new PartitionModel(), new CIIDModel()));
-            var userModel = new UserInDatabaseModel();
-            var changesetModel = new ChangesetModel(userModel);
-            var model = new CIModel(attributeModel, new CIIDModel());
-            var layerModel = new LayerModel();
             var e = new ExternalIDMapPostgresPersister();
             var p = new ScopedExternalIDMapPostgresPersister("tmp", e);
 
             var trans = ModelContextBuilder.BuildImmediate();
-            var user = await DBSetup.SetupUser(userModel, trans);
 
-            var (layer, _) = await layerModel.CreateLayerIfNotExists("l1", trans);
+            var (layer, _) = await GetService<ILayerModel>().CreateLayerIfNotExists("l1", trans);
 
             Assert.AreEqual(0, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
 
-            var ciid1 = await model.CreateCI(trans);
+            var ciid1 = await GetService<ICIModel>().CreateCI(trans);
 
             Assert.AreEqual(1, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
             Assert.AreEqual(0, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
 
-            var ciid2 = await model.CreateCI(trans);
-            var changeset1 = new ChangesetProxy(user, TimeThreshold.BuildLatest(), changesetModel);
-            await attributeModel.InsertAttribute("foo", new AttributeScalarValueText("bar"), ciid2, layer.ID, changeset1, new DataOriginV1(DataOriginType.Manual), trans);
+            var ciid2 = await GetService<ICIModel>().CreateCI(trans);
+            var changeset1 = await CreateChangesetProxy();
+            await GetService<IAttributeModel>().InsertAttribute("foo", new AttributeScalarValueText("bar"), ciid2, layer.ID, changeset1, new DataOriginV1(DataOriginType.Manual), trans);
 
             Assert.AreEqual(0, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
 
             // create another empty ci
-            var ciid3 = await model.CreateCI(trans);
+            var ciid3 = await GetService<ICIModel>().CreateCI(trans);
             Assert.AreEqual(1, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
-            Assert.AreEqual(1, (await model.GetCIIDs(trans)).Count());
-            Assert.IsTrue((await model.GetCIIDs(trans)).First() == ciid2);
+            Assert.AreEqual(1, (await GetService<ICIModel>().GetCIIDs(trans)).Count());
+            Assert.IsTrue((await GetService<ICIModel>().GetCIIDs(trans)).First() == ciid2);
             Assert.AreEqual(0, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
 
             // test archiving unused CIs with external id mapping present
-            var ciid4 = await model.CreateCI(trans);
+            var ciid4 = await GetService<ICIModel>().CreateCI(trans);
             await p.Persist(new Dictionary<Guid, string>() { { ciid4, "tmp" } }, trans);
             Assert.AreEqual(0, await ArchiveUnusedCIsService.ArchiveUnusedCIs(e, ModelContextBuilder, NullLogger.Instance));
             await p.Persist(new Dictionary<Guid, string>() { }, trans);
