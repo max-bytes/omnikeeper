75d35da60fcb20e65398fd3817f0cda8aed889cf
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-04-25 17:30:28 +0200

    Tiny change

--- backend/OKPluginInsightDiscoveryScanIngest/IngestFileController.cs
+++ backend/OKPluginInsightDiscoveryScanIngest/IngestFileController.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Logging;
 using OKPluginGenericJSONIngest;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace OKPluginInsightDiscoveryScanIngest
 {
