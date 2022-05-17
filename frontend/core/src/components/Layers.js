e0f0b714931f8bbc11f0a1e9af8e36d1cbf7dc13
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-05-17 14:14:42 +0200

    Technical frontend: implemented toggle for visibility for all layers

--- frontend/core/src/components/Layers.js
+++ frontend/core/src/components/Layers.js
@@ -15,19 +15,35 @@ function Layers(props) {
 
   const toggleLayerVisibility = useCallback((layerID, allLayers) => {
     if (layerSettings) {
-      var l = _.find(layerSettings, ls => ls.layerID === layerID);
-      if (l) {
+      var ls = _.find(layerSettings, ls => ls.layerID === layerID);
+      if (ls) {
         setLayerSettings(layerSettings.map(ls => {
           if (ls.layerID !== layerID) return ls;
           return {...ls, visible: !ls.visible };
         }));
-      } else return setLayerSettings(_.concat(layerSettings, [{layerID: layerID, visible: false, sortOffset: 0}]));
+      } else {
+        setLayerSettings(_.concat(layerSettings, [{layerID: layerID, visible: false, sortOffset: 0}]));
+      }
     } else {
       var newLayerSettings = allLayers.map(l => {
         return { layerID: l.id, sortOffset: 0, visible: l.id !== layerID }
       });
       setLayerSettings(newLayerSettings);
     }
+  }, [layerSettings, setLayerSettings]);
+
+  const toggleLayerVisibilityAll = useCallback((allLayers) => {
+    const safeLayerSettings = layerSettings ?? [];
+    const areAllInvisible = safeLayerSettings.every(ls => !ls.visible);
+    var newLayerSettings = allLayers.map(l => {
+      var ls = _.find(safeLayerSettings, ls => ls.layerID === l.id);
+      if (ls) {
+        return {...ls, visible: areAllInvisible };
+      } else {
+        return {layerID: l.id, visible: areAllInvisible, sortOffset: 0};
+      }
+    });
+    setLayerSettings(newLayerSettings);
   }, [layerSettings, setLayerSettings]);
 
   const changeLayerSortOrder = useCallback((layerIDA, layerIDB, change, allLayers) => {
@@ -71,50 +87,54 @@ function Layers(props) {
   if (data) {
     let layers = mergeSettingsAndSortLayers(data.layers, layerSettings);
 
-    return (
-      <Flipper flipKey={layers.map(a => a.id + ";" + a.visible).join(' ')}>
-      {layers.map((layer, index) => {
+    return <>
+        <div style={{paddingBottom: '5px', display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+          <Button size='small' onClick={() => toggleLayerVisibilityAll(data.layers)}>
+            <FontAwesomeIcon icon={((true) ? faEye : faEyeSlash)} fixedWidth style={{ padding: "2px"}} />
+          </Button>
+        </div>
+        <Flipper flipKey={layers.map(a => a.id + ";" + a.visible).join(' ')}>
+        {layers.map((layer, index) => {
 
-        var nextLayer = layers[index + 1];
-        var previousLayer = layers[index - 1];
+          var nextLayer = layers[index + 1];
+          var previousLayer = layers[index - 1];
 
-        const layerDescPopup = <Popover
-          placement="topRight"
-          trigger="click"
-          content={<ul style={{marginBottom: '0px'}}><li>ID: {layer.id}</li><li>Description: {layer.description}</li></ul>}
-          on='click'
-          position='top right'
-        >
-          <Button size='small'><FontAwesomeIcon fixedWidth icon={faInfo} /></Button>
-        </Popover>
+          const layerDescPopup = <Popover
+            placement="topRight"
+            trigger="click"
+            content={<ul style={{marginBottom: '0px'}}><li>ID: {layer.id}</li><li>Description: {layer.description}</li></ul>}
+            on='click'
+            position='top right'
+          >
+            <Button size='small'><FontAwesomeIcon fixedWidth icon={faInfo} /></Button>
+          </Popover>
 
-        return (
-          <Flipped key={layer.id} flipId={layer.id}>
-            <div style={{paddingBottom: '5px', display: 'flex', alignItems: 'center'}}>
-              {!layer.writable && (<FontAwesomeIcon fixedWidth icon={faBan} />)}
-              {layer.writable && (<FontAwesomeIcon fixedWidth icon={faEdit} />)}
-              &nbsp;
-              {layerDescPopup} 
-              <LayerIcon layer={layer} />
-              <span style={{flexGrow: '1', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', color: (layer.visible) ? 'unset': '#ccc'}}>{layer.id} {((layer.state !== 'ACTIVE') ? " (DEPRECATED)" : "")}</span>
-              {layer.clConfigID !== "" && (<FontAwesomeIcon icon={faCogs} fixedWidth />)}
-              {layer.onlineInboundAdapterName !== "" && (<FontAwesomeIcon icon={faPlug} fixedWidth />)}
-              &nbsp;&nbsp;
-              <Button size='small' onClick={() => toggleLayerVisibility(layer.id, data.layers)} style={{ marginRight: "0.5rem" }}>
-                <FontAwesomeIcon icon={((layer.visible) ? faEye : faEyeSlash)} fixedWidth style={{ padding: "2px"}} />
-              </Button>
-              {/* <Radio.Group size='small' style={{flexShrink: '0'}}> */}
+          return (
+            <Flipped key={layer.id} flipId={layer.id}>
+              <div style={{paddingBottom: '5px', display: 'flex', alignItems: 'center'}}>
+                {!layer.writable && (<FontAwesomeIcon fixedWidth icon={faBan} />)}
+                {layer.writable && (<FontAwesomeIcon fixedWidth icon={faEdit} />)}
+                &nbsp;
+                {layerDescPopup} 
+                <LayerIcon layer={layer} />
+                <span style={{flexGrow: '1', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', color: (layer.visible) ? 'unset': '#ccc'}}>{layer.id} {((layer.state !== 'ACTIVE') ? " (DEPRECATED)" : "")}</span>
+                {layer.clConfigID !== "" && (<FontAwesomeIcon icon={faCogs} fixedWidth />)}
+                {layer.onlineInboundAdapterName !== "" && (<FontAwesomeIcon icon={faPlug} fixedWidth />)}
+                &nbsp;&nbsp;
                 <Button size='small' disabled={!previousLayer} onClick={() => changeLayerSortOrder(layer.id, previousLayer.id, 1, data.layers)}>
                   <FontAwesomeIcon icon={faArrowAltCircleUp} fixedWidth style={{ padding: "2px"}} />
                 </Button>
                 <Button size='small' disabled={!nextLayer} onClick={() => changeLayerSortOrder(layer.id, nextLayer.id, -1, data.layers)}>
                   <FontAwesomeIcon icon={faArrowAltCircleDown} fixedWidth style={{ padding: "2px"}} />
                 </Button>
-              {/* </Radio.Group> */}
-            </div>
-          </Flipped>)
-        })}
-      </Flipper>);
+                <Button size='small' onClick={() => toggleLayerVisibility(layer.id, data.layers)} style={{ marginLeft: "0.5rem" }}>
+                  <FontAwesomeIcon icon={((layer.visible) ? faEye : faEyeSlash)} fixedWidth style={{ padding: "2px"}} />
+                </Button>
+              </div>
+            </Flipped>)
+          })}
+        </Flipper>
+      </>;
   } else if (error) {
     return "Error";
   } else {
