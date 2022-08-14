9150b0904e52830b1d1703e1923a23a4c7ee1a5e
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-08-14 14:16:19 +0200

    Added graphql fields for reading permitted layers from auth-role

--- backend/Omnikeeper/GraphQL/Types/AuthRoleTypes.cs
+++ backend/Omnikeeper/GraphQL/Types/AuthRoleTypes.cs
@@ -1,14 +1,41 @@
-﻿using GraphQL.Types;
+﻿using GraphQL.DataLoader;
+using GraphQL.Types;
 using Omnikeeper.Base.Entity;
+using Omnikeeper.Base.GraphQL;
+using Omnikeeper.Base.Model;
+using Omnikeeper.Base.Utils;
+using Omnikeeper.Utils;
+using System.Linq;
 
 namespace Omnikeeper.GraphQL.Types
 {
     public class AuthRoleType : ObjectGraphType<AuthRole>
     {
-        public AuthRoleType()
+        public AuthRoleType(IDataLoaderService dataLoaderService, ILayerDataModel layerDataModel, IAuthRolePermissionChecker authRolePermissionChecker)
         {
             Field("id", x => x.ID);
             Field(x => x.Permissions);
+
+            Field<ListGraphType<LayerDataType>>("grantsReadAccessForLayers", resolve: (context) =>
+            {
+                var userContext = (context.UserContext as OmnikeeperUserContext)!;
+                var permissions = context.Source.Permissions;
+                return dataLoaderService.SetupAndLoadAllLayers(layerDataModel, userContext.GetTimeThreshold(context.Path), userContext.Transaction)
+                .Then(layersDict =>
+                {
+                    return layersDict.Values.Where(l => authRolePermissionChecker.DoesAuthRoleGivePermission(context.Source, PermissionUtils.GetLayerReadPermission(l.LayerID)));
+                });
+            });
+            Field<ListGraphType<LayerDataType>>("grantsWriteAccessForLayers", resolve: (context) =>
+            {
+                var userContext = (context.UserContext as OmnikeeperUserContext)!;
+                var permissions = context.Source.Permissions;
+                return dataLoaderService.SetupAndLoadAllLayers(layerDataModel, userContext.GetTimeThreshold(context.Path), userContext.Transaction)
+                .Then(layersDict =>
+                {
+                    return layersDict.Values.Where(l => authRolePermissionChecker.DoesAuthRoleGivePermission(context.Source, PermissionUtils.GetLayerWritePermission(l.LayerID)));
+                });
+            });
         }
     }
 }
