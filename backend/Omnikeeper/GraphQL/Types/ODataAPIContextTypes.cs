9dd78b0eb180151311230d653787c304c1d87aa3
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-08-15 22:41:43 +0200

    Added graphql field for getting read layers of odata context

--- backend/Omnikeeper/GraphQL/Types/ODataAPIContextTypes.cs
+++ backend/Omnikeeper/GraphQL/Types/ODataAPIContextTypes.cs
@@ -1,14 +1,33 @@
-﻿using GraphQL.Types;
+﻿using GraphQL.DataLoader;
+using GraphQL.Types;
 using Omnikeeper.Base.Entity;
+using Omnikeeper.Base.GraphQL;
+using Omnikeeper.Base.Model;
+using System.Linq;
 
 namespace Omnikeeper.GraphQL.Types
 {
     public class ODataAPIContextType : ObjectGraphType<ODataAPIContext>
     {
-        public ODataAPIContextType()
+        public ODataAPIContextType(ILayerDataModel layerDataModel, IDataLoaderService dataLoaderService)
         {
             Field("id", x => x.ID);
             Field("config", x => ODataAPIContext.ConfigSerializer.SerializeToString(x.CConfig), type: typeof(StringGraphType));
+            Field<ListGraphType<LayerDataType>>("readLayers", resolve: (context) =>
+            {
+                var userContext = (context.UserContext as OmnikeeperUserContext)!;
+                var config = context.Source.CConfig;
+                var layerIDs = (config switch
+                {
+                    ODataAPIContext.ConfigV4 v4 => v4.ReadLayerset,
+                    _ => throw new System.Exception("Unknown odata config object")
+                }).ToHashSet();
+                return dataLoaderService.SetupAndLoadAllLayers(layerDataModel, userContext.GetTimeThreshold(context.Path), userContext.Transaction)
+                .Then(layersDict =>
+                {
+                    return layersDict.Values.Where(l => layerIDs.Contains(l.LayerID));
+                });
+            });
         }
     }
 }
