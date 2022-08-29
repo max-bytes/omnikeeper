eac068b60788908ee418aac1e649492fb57ea8d0
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-08-29 14:58:35 +0200

    bugfix

--- backend/Omnikeeper.Base/Model/IChangesetProxy.cs
+++ backend/Omnikeeper.Base/Model/IChangesetProxy.cs
@@ -2,6 +2,7 @@
 using Omnikeeper.Base.Entity.DataOrigin;
 using Omnikeeper.Base.Utils;
 using Omnikeeper.Base.Utils.ModelContext;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Omnikeeper.Base.Model
@@ -9,6 +10,7 @@ namespace Omnikeeper.Base.Model
     public interface IChangesetProxy
     {
         Task<Changeset> GetChangeset(string layerID, DataOriginV1 dataOrigin, IModelContext trans);
+        IEnumerable<Changeset> GetAllActiveChangesets();
         TimeThreshold TimeThreshold { get; }
         UserInDatabase User { get; }
     }
