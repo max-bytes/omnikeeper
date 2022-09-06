638cba939c6672e431dc0534b4a38b8436e6ec0a
Maximilian Csuk <maximilian.csuk@mhx-innovations.at>
2022-09-06 16:13:28 +0200

    bugfix

--- backend/PerfTests/GetMergedAttributesTest.cs
+++ backend/PerfTests/GetMergedAttributesTest.cs
@@ -37,7 +37,8 @@ namespace PerfTests
         [Params("all")]
         public string? CIIDSelection { get; set; }
 
-        [Params("Text", "TextArray", "Integer", "IntegerArray", "JSON", "JSONArray")]
+        //[Params("Text", "TextArray", "Integer", "IntegerArray", "JSON", "JSONArray")]
+        [Params("Text")]
         public string? AttributeValueType { get; set; }
 
         [Params(false)]
