bdd92a55976b9006f164d8c59346cb15e9b8a3cf
Maximilian Csuk <maximilian.csuk@gmx.at>
2022-11-03 10:34:49 +0100

    Removed System.Linq.Dynamic.Core

--- backend/Omnikeeper/Controllers/OData/TraitEntityController.cs
+++ backend/Omnikeeper/Controllers/OData/TraitEntityController.cs
@@ -19,7 +19,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 
 namespace Omnikeeper.Controllers.OData
