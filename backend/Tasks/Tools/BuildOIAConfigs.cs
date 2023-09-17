e2ea49b74c1afbd336e14b778609fd8956791197
Maximilian Csuk <maximilian.csuk@gmx.at>
2023-09-17 10:07:29 +0200

    small changes

--- backend/Tasks/Tools/BuildOIAConfigs.cs
+++ backend/Tasks/Tools/BuildOIAConfigs.cs
@@ -11,14 +11,14 @@
 //        public void BuildOmnikeeperOIAConfig()
 //        {
 //            var config = new OKPluginOIAOmnikeeper.OnlineInboundAdapter.Config(
-//                "https://mhx.registry-test.mhx.at/backend/api",
-//                "https://auth-test.mhx.at/",
-//                "mhx",
+//                "https://example.com/backend/api",
+//                "https://example.com/",
+//                "maxbytes",
 //                "landscape-omnikeeper-api",
 //                "9c98eb5f-22f0-47d1-8123-810d7d96104f",
 //                new string[] { "CMDB" },
 //                new TimeSpan(0, 1, 0),
-//                "omnikeeper_mhx"
+//                "omnikeeper_maxbytes"
 //                );
 //            var json = IOnlineInboundAdapter.IConfig.Serializer.SerializeToString(config);
 
@@ -33,7 +33,7 @@
 //            var ExternalSharepointClientSecret = "/w5tskeWck6EV2sx6Mue1tkL+dSw42VdMHlPS5plohw=";
 //            var ExternalSharepointListID = new Guid("37800a8f-0107-445b-b70b-c783ba5a5ce3");
 
-//            var config = new OKPluginOIASharepoint.Config(ExternalSharepointTenantID, "mhxconsulting.sharepoint.com", "play2", ExternalSharepointClientID,
+//            var config = new OKPluginOIASharepoint.Config(ExternalSharepointTenantID, "example.sharepoint.com", "play2", ExternalSharepointClientID,
 //                ExternalSharepointClientSecret, true, new TimeSpan(0, 1, 0), "sharepoint",
 //                new OKPluginOIASharepoint.Config.ListConfig[] { new OKPluginOIASharepoint.Config.ListConfig(ExternalSharepointListID,
 //                    new OKPluginOIASharepoint.Config.ListColumnConfig[] {
@@ -46,7 +46,7 @@
 //            var json = IOnlineInboundAdapter.IConfig.Serializer.SerializeToString(config);
 
 //            Console.WriteLine(json);
-//            // {"$type":"OKPluginOIASharepoint.Config, OKPluginOIASharepoint","tenantID":"98061435-3c72-44d1-b37a-057e21f42801","siteDomain":"mhxconsulting.sharepoint.com","site":"play2","clientID":"3d6e9642-5430-438c-b435-34d35b323b3a","clientSecret":"/w5tskeWck6EV2sx6Mue1tkL+dSw42VdMHlPS5plohw=","useCurrentForHistoric":true,"preferredIDMapUpdateRate":"00:01:00","listConfigs":[{"$type":"OKPluginOIASharepoint.Config+ListConfig, OKPluginOIASharepoint","listID":"37800a8f-0107-445b-b70b-c783ba5a5ce3","columnConfigs":[{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"MobilePhone","targetAttributeName":"user.mobile_phone"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"Company","targetAttributeName":"user.company"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"GivenName","targetAttributeName":"user.first_name"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"Surname","targetAttributeName":"user.last_name"}],"identifiableAttributes":["user.first_name","user.last_name"],"searchableLayerIDs":[1,2]}],"MapperScope":"sharepoint"}
+//            // {"$type":"OKPluginOIASharepoint.Config, OKPluginOIASharepoint","tenantID":"98061435-3c72-44d1-b37a-057e21f42801","siteDomain":"example.sharepoint.com","site":"play2","clientID":"3d6e9642-5430-438c-b435-34d35b323b3a","clientSecret":"/w5tskeWck6EV2sx6Mue1tkL+dSw42VdMHlPS5plohw=","useCurrentForHistoric":true,"preferredIDMapUpdateRate":"00:01:00","listConfigs":[{"$type":"OKPluginOIASharepoint.Config+ListConfig, OKPluginOIASharepoint","listID":"37800a8f-0107-445b-b70b-c783ba5a5ce3","columnConfigs":[{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"MobilePhone","targetAttributeName":"user.mobile_phone"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"Company","targetAttributeName":"user.company"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"GivenName","targetAttributeName":"user.first_name"},{"$type":"OKPluginOIASharepoint.Config+ListColumnConfig, OKPluginOIASharepoint","sourceColumn":"Surname","targetAttributeName":"user.last_name"}],"identifiableAttributes":["user.first_name","user.last_name"],"searchableLayerIDs":[1,2]}],"MapperScope":"sharepoint"}
 //        }
 
 
