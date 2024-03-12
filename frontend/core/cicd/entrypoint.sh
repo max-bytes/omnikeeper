0a3217fb1a210287b54ae0a8be566182a5a4682b
Maximilian Csuk <maximilian.csuk@gmx.at>
2024-03-12 17:11:23 +0100

    switched to using subfolder for generated __ENV.js file

--- frontend/core/cicd/entrypoint.sh
+++ frontend/core/cicd/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Serializing environment:"
-react-env --dest .
+react-env --dest ./env_js
 
 echo "Dynamically replacing PUBLIC_URL_PLACEHOLDER with PUBLIC_URL_DYNAMIC"
 find . -type f \( -name "*.html" -o -name "*.js*" -o -name "*.json" -o -name "*.css" \) -print0 | xargs -0 sed -i'' -e "s/__PUBLIC_URL_PLACEHOLDER__/$(echo $PUBLIC_URL_DYNAMIC | sed -e 's/[\/&]/\\&/g')/g"
